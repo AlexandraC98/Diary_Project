469fbbe93457976cf4926aeb81703d86d446011f
Alexandra Ciocirdel <alexandra.ciocirdel@icloud.com>
2024-07-14 18:22:09 +0300

    Second commit

--- Diary_Code.py
+++ Diary_Code.py
@@ -1,17 +1,28 @@
-import datetime, pytz, os, time, pwinput
+import datetime, pytz, os, time, pwinput, random, hashlib, json
 
 bucharest_tz = pytz.timezone('Europe/Bucharest')
 
 Diary={}
-
-real_psw="AlexandraC98"
+Users={}
 
 #Auto-load
 try:
   with open("Diary.txt", "r") as f:
-    Diary=eval(f.read())
+    data = json.load(f)
+    # Convert string keys back to datetime objects
+    Diary = {datetime.datetime.fromisoformat(k): v for k, v in data.get("Entries", {}).items()}
+    Users = data.get("Users", {})
 except Exception:
   pass
+
+#Auto-save
+def AutoSave():
+  with open("Diary.txt", "w") as f:
+    # Convert datetime keys to strings for JSON serialization
+    data = {
+      "Entries": {k.isoformat(): v for k, v in Diary.items()},
+      "Users": Users}
+    json.dump(data,f)
 
 #Printing option
 def printDiary():
@@ -21,6 +32,7 @@ def printDiary():
     print(f"{index+1}. {local_time.strftime('%Y-%m-%d %H:%M:%S')} > {Diary[key]['title']}")
   print()
 
+###Entries
 #Add entry
 def Add():
   os.system("clear")
@@ -30,6 +42,7 @@ def Add():
   Diary[key]={"title":title,"entry":entry}
   print("Entry added.")
   time.sleep(1)
+  AutoSave()
 
 #View entry
 def View():
@@ -39,14 +52,14 @@ def View():
     return
 
   index=0
-  
+
   while True:
     os.system("clear")
     local_time=sorted_keys[index].astimezone(bucharest_tz)
     print(f"{local_time.strftime('%Y-%m-%d')}: {Diary[sorted_keys[index]]}", end="\n")
     time.sleep(1)
-  
-    choice=input(f"1. Next entry\n2. Previous entry\n3. Exit\n> ")
+
+    choice=input("1. Next entry\n2. Previous entry\n3. Exit\n> ")
 
     if choice=="1":
       index=(index-1)
@@ -60,31 +73,27 @@ def View():
 #Remove entry
 def Remove():
   os.system("clear")
-  
+
   printDiary()
 
   try:
     user_index=int(input("Which entry do you wish to remove? > ")) - 1
-    
+
     if user_index<0 or user_index>=len(Diary):
       print("Invalid index. Please enter the correct index.")
-      
+
     else:
       removed_key = sorted(Diary.keys(), reverse=True)[user_index]
       del Diary[removed_key]
-      print(f"Entry deleted.")
+      print("Entry deleted.")
       print()
       printDiary()
       time.sleep(1)
+      AutoSave()
 
   except ValueError:
     print("Invalid input. Please enter a number.")
     time.sleep(1)
-
-#Auto-save
-def AutoSave():
-  with open("Diary.txt", "w") as f:
-    f.write(str(Diary))
 
 #Exit the diary
 def Quit():
@@ -94,24 +103,78 @@ def Quit():
     return True
   return False
 
+#Subroutine to hash and salt
+def hash_with_salt(text, salt):
+  return hashlib.sha256(f"{text}{salt}".encode()).hexdigest()
+
+###Users
+#Add diary user
+def AddUser():
+  os.system("clear")
+  username = pwinput.pwinput("Username: ")
+  password = pwinput.pwinput("Password: ")
+
+  salt = str(random.randint(1000, 9999999999999))
+
+  newUser = hash_with_salt(username, salt)
+  newPass = hash_with_salt(password, salt)
+
+  Users[newUser] = {"password": newPass, "salt": salt}
+
+  print()
+  
+  print("\033[35m User added. \033[0m")
+  time.sleep(1)
+  
+  AutoSave()
+
+#Login to your diary
+def LoginUser():
+  os.system("clear")
+  username = pwinput.pwinput("Username: ")
+
+  for user in Users:
+    salt = Users[user]["salt"]
+    newUser = hash_with_salt(username, salt)
+
+    if newUser in Users:
+      password = pwinput.pwinput("Password: ")
+      newPass = hash_with_salt(password, salt)
+
+      if newPass == Users[newUser]["password"]:
+        print("\033[35m Login Successful. \033[0m")
+        time.sleep(1)
+        return True
+      else:
+         print("Login Failed")
+         return False
+
+  print("\033[33m Username not found. \033[0m")
+  time.sleep(1)
+  exit()
+
+#Main Loop
 while True:
   os.system("clear")
-  psw=pwinput.pwinput("Enter your password: ")
-  if psw!=real_psw:
-    break
-  else:
-    while True:
-      os.system("clear")
-      menu=input("1. Add entry\n2. View entry\n3. Remove entry\n> ")
-      if menu=="1":
-        Add()
-      elif menu=="2":
-        View()
-      elif menu=="3":
-        Remove()
+  menu1=input("1. Add user\n\n2. Login to your diary\n\n> ")
+
+  if menu1=="1":
+    AddUser()
+  elif menu1=="2":
+    if LoginUser():
+    
+      while True:
+        os.system("clear")
+        menu=input("1. Add entry\n\n2. View entry\n\n3. Remove entry\n\n> ")
+        if menu=="1":
+          Add()
+        elif menu=="2":
+          View()
+        elif menu=="3":
+          Remove()
   
-      if Quit():
-        break
+        if Quit():
+          break
 
   user_input=input("Do you want to exit? (y/n) ")
   if user_input.lower().startswith("y"):
